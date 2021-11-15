94903758919cb61b4badc3db2367937807ab08b0
ahmedadel56 <ahmdadl563@gmail.com>
2021-11-15 18:59:16 +0200

    creat files structre and nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-function App() {
-  return (
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
+import Profile from './components/Profile';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+
+const App = () => (
+  <BrowserRouter>
     <div className="App">
-      <ul className="nav justify-content-end" />
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Rockets />
+        </Route>
+        <Route path="/missions">
+          <Missions />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
     </div>
-  );
-}
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from '../icon.png';
+
+const Nav = () => (
+  <header>
+    <span className="head">
+      <img src={Logo} alt="logo" />
+      <h1>
+        {'Space Travelers\'s Hub'}
+      </h1>
+    </span>
+    <nav>
+      <NavLink to="/" className="NavLink" activeClassName="is-active" exact>
+        <span className="rockets">Rockets</span>
+      </NavLink>
+      <NavLink
+        to="/misssions"
+        className="NavLink"
+        activeClassName="is-active"
+      >
+        <span className="missions ">Missions</span>
+      </NavLink>
+      <NavLink to="/profile" className="NavLink" activeClassName="is-active">
+        <span className="profile ">My profile</span>
+      </NavLink>
+    </nav>
+  </header>
+);
+
+export default Nav;
