0582dd0b172b04f3ddf2ac0864713d2655726c94
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-11-16 19:02:43 +0200

    Fix fetch error

--- src/redux/missions.js
+++ src/redux/missions.js
@@ -1,27 +1,28 @@
 const GET_MISSIONS_SUCCESS = 'spaceTravelers/rockets/GET_MISSIONS_SUCCESS';
 const REGISTER_MISSIONS = 'spaceTravelers/rockets/REGISTER_MISSIONS';
 const CANCEL_MISSIONS = 'spaceTravelers/rockets/CANCEL_MISSIONS';
+const API_URL = 'https://api.spacexdata.com/v3/missions';
 
 export const initState = [];
 
-export const getMisstions = async (dispatch) => {
-  const response = await fetch('https://api.spacexdata.com/v3/missions');
-  const missions = response.json();
-  const missoinslist = [];
-
-  missions.forEach((mission) => {
-    const missionsDetails = {
-      mission_id: mission.mission_id,
-      mission_name: mission.mission_name,
-      description: mission.description,
-    };
-    missoinslist.push(missionsDetails);
-  });
-
-  dispatch({
-    type: GET_MISSIONS_SUCCESS,
-    payload: missoinslist,
-  });
+export const getMisstions = () => async (dispatch) => {
+  await fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const missions = [];
+      data.forEach((el) => {
+        missions.push({
+          mission_id: el.mission_id,
+          mission_name: el.mission_name,
+          description: el.description,
+          reserved: false,
+        });
+      });
+      dispatch({
+        type: GET_MISSIONS_SUCCESS,
+        payload: missions,
+      });
+    });
 };
 
 export const registermissions = (id) => ({
@@ -37,7 +38,7 @@ export const cancelmission = (id) => ({
 const MissionReducer = (state = initState, action) => {
   switch (action.type) {
     case GET_MISSIONS_SUCCESS:
-      return [...action.payload];
+      return action.payload;
     case REGISTER_MISSIONS:
       return state.map((mission) => {
         if (mission.id !== action.id) {
