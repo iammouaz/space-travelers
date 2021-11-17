216270828e79fe634e9dc9ebdd55917fed560c9a
ahmedadel56 <ahmdadl563@gmail.com>
2021-11-17 19:20:40 +0200

    Style Rockets section

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,23 +5,23 @@ import Logo from '../icon.png';
 const Nav = () => (
   <header className="header">
     <span className="head">
-      <img className="img" src={Logo} alt="logo" />
+      <img className="img navbar-brand" src={Logo} alt="logo" />
       <h1>
         {'Space Travelers\'s Hub'}
       </h1>
     </span>
-    <nav className="nav">
-      <NavLink to="/" className="NavLink" activeClassName="is-active" exact>
+    <nav className="nav justify-content-end">
+      <NavLink to="/" className="nav-link" activeClassName="is-active" exact>
         <span className="rockets">Rockets</span>
       </NavLink>
       <NavLink
         to="/missions"
-        className="NavLink"
+        className="nav-link"
         activeClassName="is-active"
       >
         <span className="missions ">Missions</span>
       </NavLink>
-      <NavLink to="/profile" className="NavLink profile" activeClassName="is-active">
+      <NavLink to="/profile" className="nav-link profile" activeClassName="is-active">
         <span>My profile</span>
       </NavLink>
     </nav>
