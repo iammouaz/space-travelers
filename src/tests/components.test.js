ccebe598ad2dec5aed151bb5073ff45ab07a9177
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-11-18 16:48:50 +0200

    Add Rockets Testing

--- src/tests/components.test.js
+++ src/tests/components.test.js
@@ -4,11 +4,12 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from '../redux/configureStore';
 import MissionReducer from '../redux/missions';
-
+import rocketsRducer from '../redux/rockets';
 // Components
 import Profile from '../components/Profile';
 import Missions from '../components/Missions';
 import Nav from '../components/Nav';
+import Rockets from '../components/Rockets';
 
 describe('Components Matches', () => {
   it('Mission Components Matches', () => {
@@ -34,5 +35,17 @@ describe('Test Nav renderisation', () => {
       .create(<BrowserRouter><Nav /></BrowserRouter>)
       .toJSON();
     expect(nav).toMatchSnapshot();
+  });
+});
+
+describe('Components Rockets', () => {
+  it('Missions Components Matches', () => {
+    const rockets = renderer
+      .create(<Provider store={store}><Rockets /></Provider>)
+      .toJSON();
+    expect(rockets).toMatchSnapshot();
+  });
+  test('Check the initialState of the reducer', () => {
+    expect(rocketsRducer(undefined, {})).toEqual([]);
   });
 });
